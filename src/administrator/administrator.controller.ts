f2123849d900b80671e062a93dda50f1777a5e31
Marko Todic <todic.marko99@gmail.com>
2023-01-02 19:48:09 +0100

    Implement the mechanism to add and edit administrators

--- src/administrator/administrator.controller.ts
+++ src/administrator/administrator.controller.ts
@@ -1,6 +1,8 @@
 import { Administrator } from './administrator.entity';
 import { AdministratorService } from './administrator.service';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { AddAdministratorDto } from './dto/add-administrator.dto';
+import { EditAdministratorDto } from './dto/edit-administrator.dto';
 
 @Controller('administrator')
 export class AdministratorController {
@@ -14,5 +16,23 @@ export class AdministratorController {
   @Get('/:id')
   getById(@Param('id') id: number): Promise<Administrator> {
     return this.administratorService.getById(id);
+  }
+
+  @Post()
+  addAdministrator(
+    @Body() addAdministratorDto: AddAdministratorDto,
+  ): Promise<Administrator> {
+    return this.administratorService.addAdministrator(addAdministratorDto);
+  }
+
+  @Put('/:id')
+  editAdministrator(
+    @Param('id') id: number,
+    @Body() editAdministratorDto: EditAdministratorDto,
+  ): Promise<Administrator> {
+    return this.administratorService.editAdministrator(
+      id,
+      editAdministratorDto,
+    );
   }
 }
--- src/administrator/administrator.service.ts
+++ src/administrator/administrator.service.ts
@@ -2,6 +2,9 @@ import { Administrator } from './administrator.entity';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AddAdministratorDto } from './dto/add-administrator.dto';
+import * as bcrypt from 'bcrypt';
+import { EditAdministratorDto } from './dto/edit-administrator.dto';
 
 @Injectable()
 export class AdministratorService {
@@ -28,5 +31,28 @@ export class AdministratorService {
     }
 
     return administrator;
+  }
+
+  async addAdministrator(
+    addAdministratorDto: AddAdministratorDto,
+  ): Promise<Administrator> {
+    const { password } = addAdministratorDto;
+    const administrator =
+      this.administratorRepository.create(addAdministratorDto);
+
+    administrator.passwordHash = await bcrypt.hash(password, 10);
+
+    return await this.administratorRepository.save(administrator);
+  }
+
+  async editAdministrator(
+    id: number,
+    editAdministratorDto: EditAdministratorDto,
+  ): Promise<Administrator> {
+    const administrator = await this.getById(id);
+
+    administrator.passwordHash = bcrypt.hash(editAdministratorDto.password, 10);
+
+    return await this.administratorRepository.save(administrator);
   }
 }
