5e9fb2b99739f06f179b97119a0db0974f23334b
Marko Todic <todic.marko99@gmail.com>
2023-01-10 18:07:18 +0100

    Implement statically served photos

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { StorageConfig } from './shared/config/storage.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.setGlobalPrefix('api', {
   //   exclude: [{ path: '/api/auth/', method: RequestMethod.ALL }],
   // });
+  app.useStaticAssets(StorageConfig.photo.destination, {
+    prefix: StorageConfig.photo.urlPrefix,
+    maxAge: StorageConfig.photo.maxAge,
+    index: false,
+  });
 
   await app.listen(3000);
 }
--- src/shared/config/storage.config.ts
+++ src/shared/config/storage.config.ts
@@ -2,7 +2,9 @@
 export const StorageConfig = {
   photo: {
     destination: '../it-store-storage/photos/',
+    urlPrefix: '/assets/photos',
     maxSize: 3 * 1024 * 1024,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
     resize: {
         thumb: {
             width: 120, 
